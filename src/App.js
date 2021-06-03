0909f28c5c9c00731fcf45e764fd9cab7ecdac92
kais <ing.amiri.kais@gmail.com>
2021-06-03 13:12:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MovieList from "./components/moviesList/MovieList";
+import { myMovies } from "./Data/Data";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Star from "./components/starComponent/Star";
 
 function App() {
+  const fiveStars = 5;
+  const [Movies, setmyMovie] = useState(myMovies);
+  const [inputSearch, setInputSearch] = useState("");
+  const [starSearch, setStarSearch] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <input type="text" onChange={(e) => setInputSearch(e.target.value)} />
+        {[...Array(fiveStars)].map((e, i) => (
+          <Star key={i} setStarSearch={setStarSearch} i={i} />
+        ))}
       </header>
+      {}
+      <MovieList
+        myMovies={Movies}
+        setmyMovie={setmyMovie}
+        inputSearch={inputSearch}
+        starSearch={starSearch}
+      />
     </div>
   );
 }
